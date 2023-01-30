3ada24e4b4e2413b40f1ba72a00f0039dd750504
Valto Silva <valtosilva7@gmail.com>
2023-01-30 19:24:50 -0300

    03 Add concurrently to run frontend and backend, Added Task, TaskList, TaskForm components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import TaskList from "./components/TaskList";
+
 function App() {
   return (
-    <div>
-      <h1>Hello REACT JS!</h1>
+    <div className="app">
+      <div className="task-container">
+        <TaskList />
+      </div>
+      <ToastContainer />
     </div>
   );
 }
